22a9edf9b052a084835539e7320660bb79ac273f
John Novakowski <jnovakowski797@gmail.com>
2023-11-27 11:58:56 -0500

    Vanilla Framework Blog Cards App Starting Point

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Chatroom from './Chatroom.jsx';
 
 const App = () => {
 
   return (
     <div className='app'>
-      <h1>Firebase Testing Grounds</h1>
-      <Chatroom/>
+      <h1>Boiler Plate</h1>
     </div>
   )
 };
