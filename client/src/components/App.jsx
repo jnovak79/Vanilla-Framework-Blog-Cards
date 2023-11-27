e4906ec15da35b884c5f2a66470eab467a8d70f5
John Novakowski <jnovakowski797@gmail.com>
2023-11-27 11:53:12 -0500

    boiler plate set-up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chatroom from './Chatroom.jsx';
+
+const App = () => {
+
+  return (
+    <div className='app'>
+      <h1>Firebase Testing Grounds</h1>
+      <Chatroom/>
+    </div>
+  )
+};
+
+export default App;
