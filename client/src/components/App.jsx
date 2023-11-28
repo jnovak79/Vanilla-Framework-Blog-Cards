41a5fbf1caec9086e5dd8edf63d7e60dc7fd214b
John Novakowski <jnovakowski797@gmail.com>
2023-11-27 23:26:21 -0500

    initial completion of both functional and styling requirements

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ArticleCard from './ArticleCard.jsx';
 
 const App = () => {
 
+  const [articleInfo, setArticleInfo] = useState([]);
+
+  console.log(articleInfo);
+
   useEffect(function() {
     let url = '/blogs';
 
     axios.get(url)
-      .then((result) => {console.log(result)})
+      .then((result) => {
+        setArticleInfo(result.data);
+      })
       .catch((error) => {console.log(error)});
   }, []);
 
 
   return (
     <div className='app'>
-      <h1>Blog Cards</h1>
-      <ArticleCard />
+      <div className='row'>
+        {articleInfo.map(function(article, index) {
+          return (
+            <ArticleCard key={index} article={article}/>
+          )
+        })}
+      </div>
     </div>
   )
 };
--- client/src/components/ArticleCard.jsx
+++ client/src/components/ArticleCard.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
 
-const ArticleCard = () => {
+const ArticleCard = ({article}) => {
 
+  const date = new Date(article.date);
+  const options = { day: 'numeric', month: 'long', year: 'numeric' };
+  const day = date.toLocaleDateString('en-US', { day: '2-digit' });
+  const month = date.toLocaleDateString('en-US', { month: 'long' });
+  const year = date.toLocaleDateString('en-US', { year: 'numeric' });
+  const formattedDate = `${day} ${month} ${year}`;
 
   return (
-    <div className="p-card--highlighted">
-      <h2>Placeholder Header</h2>
-      <hr className="u-sv1"></hr>
-      <h3>Placeholder Title</h3>
-      <p className="p-card__content">Placeholder footnote</p>
-      <hr className="u-sv1"></hr>
-      <p>Placeholder Footnote</p>
+    <div className='col'>
+      <div className="p-card--highlighted">
+        <p className="p-card__content">CLOUD AND SERVER</p>
+        <hr className="u-sv1"></hr>
+        <a href={article.link}><img className="p-card__image" src={article.featured_media}/></a>
+        <a href={article.link}><h1>{article.title.rendered}</h1></a>
+        <div className='bottomCardPortion'>
+          <p className="p-card__content"><em>By <a href={article._embedded.author[0].link}>{article._embedded.author[0].name}</a> on {formattedDate}</em></p>
+          <hr className="u-sv1"></hr>
+          <p>Article</p>
+        </div>
+      </div>
   </div>
   )
 }
